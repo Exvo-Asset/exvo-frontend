159c0cf1c3246377c7104904f38259419ebf8449
Ankith <160985511+zaftus@users.noreply.github.com>
2025-11-09 23:06:17 -0500

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Dashboard from './components/Dashboard'
+
+
+export default function App(){
+return <div style={{padding:20}}><h1>Exvo Assets</h1><Dashboard/></div>
+}
